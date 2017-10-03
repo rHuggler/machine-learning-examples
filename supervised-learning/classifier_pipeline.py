a2e05d92518edb4c7226c255e1f2a002f477d8d4
Ronald Huggler <ronald@huggler.com.br>
2017-10-03 15:22:51 -0300

    Add a more complete classifier example
    Add a better example of classifier pipeline with training and testing data separation, prints the accuracy score at the end

---
+++ supervised-learning/classifier_pipeline.py
@@ -0,0 +1,23 @@
+from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+# Loads iris dataset
+iris = load_iris()
+
+# Uppercase for matrix, lowercase for vector
+X = iris.data
+y = iris.target
+
+# Split our dataset by 50%/50%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+
+# Store predictions to variable
+predictions = classifier.predict(X_test)
+
+# Print our predictions accuracy compared to our test targets
+print(accuracy_score(y_test, predictions))
