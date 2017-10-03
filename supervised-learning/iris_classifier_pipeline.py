98fd4ba7840f938a40867a649838e9cf37b51c3a
Ronald Huggler <ronald@huggler.com.br>
2017-10-03 15:49:31 -0300

    Change classifier to K-Nearest-Neighbors

--- supervised-learning/iris_classifier_pipeline.py
+++ supervised-learning/iris_classifier_pipeline.py
@@ -1,4 +1,4 @@
-from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -13,7 +13,7 @@ y = iris.target
 # Split our dataset by 50%/50%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
-classifier = tree.DecisionTreeClassifier()
+classifier = KNeighborsClassifier()
 classifier.fit(X_train, y_train)
 
 # Store predictions to variable
